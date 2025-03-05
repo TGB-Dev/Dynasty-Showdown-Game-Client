964d2e9941c35de0713863f48ad094c5cd7aabc6
Binh Minh <bmmit.work@gmail.com>
2025-03-05 19:56:10 +0700

    feat(base): setup dependencies, eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import prettierRecommended from "eslint-plugin-prettier/recommended"
+import love from "eslint-config-love";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +12,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["src/components/ui/*"]
+  },
+
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  love,
+  prettierRecommended,
+
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+    }
+  }
 ];
 
 export default eslintConfig;

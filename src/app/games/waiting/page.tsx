94acb4889c5e03797681f39b8791ab95f3a6c9f1
KhuiKhui <khoikhuat0807@gmail.com>
2025-04-06 21:38:03 +0700

    feat: add in-game and no-game waiting room

---
+++ src/app/games/waiting/page.tsx
@@ -0,0 +1,24 @@
+import { Game } from "@/types/games.enum";
+import { Box, Flex } from "@chakra-ui/react";
+function InGameWaitingRoom() {
+  const currentGame: () => string = () => {
+    return Game.MatChieuHoangGia;
+  };
+  return (
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center"
+      padding={4}
+    >
+      <Box>
+        Bạn đang trong phòng chờ của <b>{currentGame()}</b>, vui lòng đợi chốc
+        lát.
+      </Box>
+    </Flex>
+  );
+}
+
+export default InGameWaitingRoom;
